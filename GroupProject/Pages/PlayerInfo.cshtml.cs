bbe69c3402283fcd87c638405642fca34b17d474
ShukiSaito <saitosi@mail.uc.edu>
2020-11-27 05:16:34 -0500

    Validation

--- GroupProject/Pages/PlayerInfo.cshtml.cs
+++ GroupProject/Pages/PlayerInfo.cshtml.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 using QuickType;
 using QuickTypePlayerStats;
 
@@ -16,19 +18,46 @@ namespace GroupProject.Pages
         {
             using (var webClient = new WebClient())
             {
-                string playersStats = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/stats/json/PlayerSeasonStats/383?key=bc49021bad1943008414c5a75e665961");
+                string InfoString = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/stats/json/PlayerSeasonStats/383?key=bc49021bad1943008414c5a75e665961");
 
+                JSchema InfoSchema = JSchema.Parse(System.IO.File.ReadAllText("PlayerInfoSchema.json"));
+                JArray InfoArray = JArray.Parse(InfoString);
+                IList<string> validationInfo = new List<string>();
+                if (InfoArray.IsValid(InfoSchema, out validationInfo))
+                {
+                    var playerInfo = PlayerInfo.FromJson(InfoString);
 
-                var playerstat = PlayerInfo.FromJson(playersStats);
+                    ViewData["PlayerInfo"] = playerInfo;
+                } 
+                else
+                {
+                    foreach(string evtInfo in validationInfo)
+                    {
+                        Console.WriteLine(evtInfo);
+                    }
+                    ViewData["PlayerInfo"] = new List<PlayerInfo>();
+                }
 
-                ViewData["PlayerInfo"] = playerstat;
+               
 
-                string playerall = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/scores/json/MembershipsByCompetition/EPL?key=bc49021bad1943008414c5a75e665961");
+                string StatsString = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/scores/json/MembershipsByCompetition/EPL?key=bc49021bad1943008414c5a75e665961");
+                JSchema Statsschema = JSchema.Parse(System.IO.File.ReadAllText("PlayerStatsSchema.json"));
+                JArray StatArray = JArray.Parse(StatsString);
+                IList<string> validationStats = new List<string>();
+                if (StatArray.IsValid(Statsschema, out validationStats))
+                {
+                    var playerStats = PlayerStats.FromJson(StatsString);
 
-
-                var playeralls = PlayerStats.FromJson(playerall);
-
-                ViewData["PlayerStats"] = playeralls;
+                    ViewData["PlayerStats"] = playerStats;
+                }
+                else
+                {
+                    foreach(string evtStats in validationStats)
+                    {
+                        Console.WriteLine(evtStats);
+                    }
+                    ViewData["PlayerStats"] = new List<PlayerStats>();
+                }
 
 
             }
--- GroupProject/Pages/PlayerSalary.cshtml.cs
+++ GroupProject/Pages/PlayerSalary.cshtml.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 using QuickTypePlayerStats;
 using QuickTypeSalary;
 using QuickTypeSalary2;
@@ -17,22 +19,69 @@ namespace GroupProject.Pages
         {
             using (var webClient = new WebClient())
             {
-                string playersSalary = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/projections/json/PlayerGameProjectionStatsByCompetition/EPL/2020-11-21?key=bc49021bad1943008414c5a75e665961");
-                string playersSalary2 = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/projections/json/PlayerGameProjectionStatsByCompetition/EPL/2020-11-22?key=bc49021bad1943008414c5a75e665961");
-                
-                var playerSalary = PlayerSalary.FromJson(playersSalary);
-                var playerSalary2 = PlayerSalary2.FromJson(playersSalary2);
-                
+                string SalaryString = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/projections/json/PlayerGameProjectionStatsByCompetition/EPL/2020-11-21?key=bc49021bad1943008414c5a75e665961");
+                JSchema SalarySchema = JSchema.Parse(System.IO.File.ReadAllText("PlayerSalarySchema.json"));
+                JArray SalaryArray = JArray.Parse(SalaryString);
+                IList<string> validationSalary = new List<string>();
+                if (SalaryArray.IsValid(SalarySchema, out validationSalary))
+                {
+                    var playerSalary = PlayerSalary.FromJson(SalaryString);
 
-                ViewData["PlayerSalary"] = playerSalary;
-                ViewData["PlayerSalary2"] = playerSalary2;
+                    ViewData["PlayerSalary"] = playerSalary;
+                }
+                else
+                {
+                    foreach (string evtSalary in validationSalary)
+                    {
+                        Console.WriteLine(evtSalary);
+                    }
+                    ViewData["PlayerSalary"] = new List<PlayerSalary>();
+                }
 
-                string playerall = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/scores/json/MembershipsByCompetition/EPL?key=bc49021bad1943008414c5a75e665961");
 
 
-                var playeralls = PlayerStats.FromJson(playerall);
 
-                ViewData["PlayerStats"] = playeralls;
+                string Salary2String = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/projections/json/PlayerGameProjectionStatsByCompetition/EPL/2020-11-22?key=bc49021bad1943008414c5a75e665961");
+
+                JSchema Salary2Schema = JSchema.Parse(System.IO.File.ReadAllText("PlayerSalary2Schema.json"));
+                JArray Salary2Array = JArray.Parse(Salary2String);
+                IList<string> validationSalary2 = new List<string>();
+                if (Salary2Array.IsValid(Salary2Schema, out validationSalary2))
+                {
+                    var playerSalary2 = PlayerSalary2.FromJson(Salary2String);
+
+                    ViewData["PlayerSalary2"] = playerSalary2;
+                }
+                else
+                {
+                    foreach (string evtSalary2 in validationSalary2)
+                    {
+                        Console.WriteLine(evtSalary2);
+                    }
+                    ViewData["PlayerSalary2"] = new List<PlayerSalary2>();
+                }
+
+
+
+
+                string StatsString = webClient.DownloadString("https://api.sportsdata.io/v3/soccer/scores/json/MembershipsByCompetition/EPL?key=bc49021bad1943008414c5a75e665961");
+                JSchema Statsschema = JSchema.Parse(System.IO.File.ReadAllText("PlayerStatsSchema.json"));
+                JArray StatArray = JArray.Parse(StatsString);
+                IList<string> validationStats = new List<string>();
+                if (StatArray.IsValid(Statsschema, out validationStats))
+                {
+                    var playerStats = PlayerStats.FromJson(StatsString);
+
+                    ViewData["PlayerStats"] = playerStats;
+                }
+                else
+                {
+                    foreach (string evtStats in validationStats)
+                    {
+                        Console.WriteLine(evtStats);
+                    }
+                    ViewData["PlayerStats"] = new List<PlayerStats>();
+                }
 
 
             }
